c9c6070f3786eab010dcb934184fcd9065003e18
Vick Manjarrez <vick.manjarrez@gmail.com>
2020-11-11 01:14:23 -0600

    deploying

--- Develop/server.js
+++ Develop/server.js
@@ -26,7 +26,7 @@ mongoose.connect(
 );
 
 // routes
-app.use(require("./routes/api.js"));
+app.use(require("./routes/api.js")); 
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
