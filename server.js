a90820690497b670eaf4275679557be4077ed97a
Vick Manjarrez <vick.manjarrez@gmail.com>
2020-11-12 22:56:24 -0600

    updated server.js

--- server.js
+++ server.js
@@ -19,6 +19,8 @@ mongoose.connect(
   process.env.MONGODB_URI || 'mongodb://localhost/budgettracker',
   {
     useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
     useFindAndModify: false
   }
 );
